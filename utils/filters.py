d3a72f3022d761ccf02fd7c06558287bfddc5c9c
zhangxiaorun <zhangxiaorun@qvod.com>
2014-03-29 19:17:34 +0800

    模板中添加过滤器

---
+++ utils/filters.py
@@ -0,0 +1,18 @@
+# coding=utf8
+"""
+jinja2的过滤器
+"""
+import markdown
+
+
+def md2html(md):
+    """
+    @param {unicode} md
+    @return {unicode html}
+    """
+    return markdown.markdown(md, ['extra', 'codehilite', 'toc'], safe_mode="escape")
+
+
+JINJA2_FILTERS = {
+    'md2html': md2html,
+}
