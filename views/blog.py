a577235a1ab080df4df3fa7983332741a24b3410
gausszh <gauss.zh@gmail.com>
2014-04-10 01:02:12 +0800

    修改处理草稿的逻辑

--- views/blog.py
+++ views/blog.py
@@ -1,10 +1,9 @@
-#coding=utf8
+# coding=utf8
 
 import datetime
 import urllib
-import markdown
-from flask import Blueprint, request, jsonify, render_template, make_response, g
-import flask_login 
+from flask import Blueprint, request, jsonify, render_template
+import flask_login
 from sae.storage import Bucket
 
 from models.blog import create_session, BlogArticle
@@ -15,93 +14,90 @@ bp_blog = Blueprint('blog', __name__, url_prefix='/blog')
 bucket = Bucket(settings.STORAGE_BUCKET_DOMAIN_NAME)
 bucket.put()
 
+
 @bp_blog.route('/')
 @bp_blog.route('/list/')
 def list():
-	session = create_session()
-	blogs = session.query(BlogArticle).filter_by(is_active=1).order_by(BlogArticle.update_time.desc()).all()
-	session.close()
-	return render_template('blog/blog_list.html', blogs=blogs)
+    session = create_session()
+    blogs = session.query(BlogArticle).filter_by(is_active=1)\
+        .order_by(BlogArticle.update_time.desc()).all()
+    session.close()
+    return render_template('blog/blog_list.html', blogs=blogs)
 
 
 @bp_blog.route('/edit/<int:blog_id>/', methods=['GET', 'POST'])
 @bp_blog.route('/edit/', methods=['GET', 'POST'])
 @flask_login.login_required
 def edit(blog_id=0):
-	if request.method == 'GET':
-		if blog_id == 0:
-			blog = None
-		else:
-			session = create_session()
-			blog = session.query(BlogArticle).filter_by(id=blog_id).first()
-			session.close()
-		return render_template('blog/blog_edit.html', blog=blog)
+    if request.method == 'GET':
+        if blog_id == 0:
+            blog = None
+        else:
+            session = create_session()
+            blog = session.query(BlogArticle).filter_by(id=blog_id).first()
+            session.close()
+        return render_template('blog/blog_edit.html', blog=blog)
 
-	if request.method == 'POST':
-		form = request.form
-		markdown = form.get('markdown')
-		html = form.get('html')
-		title = form.get('title')
-		blog_id = form.get('blog_id')
-		if markdown and html and title and \
-			(len(markdown.strip()) * len(markdown.strip()) * len(markdown.strip()) > 0):
+    if request.method == 'POST':
+        form = request.form
+        markdown = form.get('markdown')
+        title = form.get('title')
+        blog_id = form.get('blog_id')
+        if markdown and title and (len(markdown.strip()) * 
+                                   len(title.strip()) > 0):
 
-			session = create_session()
-			now = datetime.datetime.now()
-			# blog_id belong to this user
-			if blog_id:
-				blog = session.query(BlogArticle).filter_by(id=blog_id).first()
-			if not blog_id or not blog:
-				blog = BlogArticle()
-				blog.create_by = 'gausszh'
-				blog.create_time = now
+            session = create_session()
+            now = datetime.datetime.now()
+            # blog_id belong to this user
+            if blog_id:
+                blog = session.query(BlogArticle).filter_by(id=blog_id).first()
+            if not blog_id or not blog:
+                blog = BlogArticle()
+                blog.create_by = flask_login.current_user.id
+                blog.create_time = now
 
-			blog.update_time = now
-			blog.title = title
-			blog.markdown = markdown
-			blog.html = html
-			session.add(blog)
-			session.commit()
-			blog_id = blog.id
-			session.close()
-			return jsonify(ok=True, data={'blog_id': blog_id})
-		return jsonify(ok=False, reason=u'数据错误')
+            blog.update_time = now
+            blog.title = title
+            blog.markdown = markdown
+            session.add(blog)
+            session.commit()
+            blog_id = blog.id
+            session.close()
+            return jsonify(ok=True, data={'blog_id': blog_id})
+        return jsonify(ok=False, reason=u'数据错误')
 
 
 @bp_blog.route('/view/<int:blog_id>/')
 def view_blog(blog_id):
-	session = create_session()
-	blog = session.query(BlogArticle).filter_by(id=blog_id).first()
-	session.close()
-	return render_template('blog/blog_view.html', blog=blog)
+    session = create_session()
+    blog = session.query(BlogArticle).filter_by(id=blog_id).first()
+    session.close()
+    return render_template('blog/blog_view.html', blog=blog)
+
 
 @bp_blog.route('/files/', methods=['POST'])
 def save_file():
-	"""
-	存储上传的图片
-	"""
-	files_name = request.files.keys()
-	ret = []
-	for fn in files_name:
-		#暂未做安全校验 PIL
-		img_file = request.files.get(fn)
-		bucket.put_object(fn, img_file)
-		link = bucket.generate_url(fn)
-		ret.append({'name': fn, 'link': link})
-	http_files_link = request.form.keys()
-	for fn in http_files_link:
-		http_link = request.form.get(fn)
-		img_file = urllib.urlopen(http_link)
-		bucket.put_object(fn, img_file)
-		link = bucket.generate_url(fn)
-		ret.append({'name': fn, 'link': link})
-	return jsonify(ok=True, data=ret)
-
-		
-		
+    """
+    存储上传的图片
+    """
+    files_name = request.files.keys()
+    ret = []
+    for fn in files_name:
+        # 暂未做安全校验 PIL
+        img_file = request.files.get(fn)
+        bucket.put_object(fn, img_file)
+        link = bucket.generate_url(fn)
+        ret.append({'name': fn, 'link': link})
+    http_files_link = request.form.keys()
+    for fn in http_files_link:
+        http_link = request.form.get(fn)
+        img_file = urllib.urlopen(http_link)
+        bucket.put_object(fn, img_file)
+        link = bucket.generate_url(fn)
+        ret.append({'name': fn, 'link': link})
+    return jsonify(ok=True, data=ret)
 
 
 @bp_blog.route('/test/')
 def test():
-	return render_template('%s.html' % request.args.get('p'))
-
+    return render_template('%s.html' % request.args.get('p'))
